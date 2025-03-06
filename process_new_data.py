694ae319aa650e224650f0e7c9abbeaa63342327
Chris Egersdoerfer <cegersdoerfer@gmail.com>
2025-03-06 16:03:46 -0500

    add logs

--- process_new_data.py
+++ process_new_data.py
@@ -406,7 +406,7 @@ def create_samples(data, time_window_size, test_size, devices):
                     # convert start_time and end_time to pd.Timestamp
                     start_time = pd.Timestamp.fromtimestamp(start_time)
                     end_time = pd.Timestamp.fromtimestamp(end_time)
-                    #print(f"start_time: {start_time}, end_time: {end_time}")
+                    print(f"start_time: {start_time}, end_time: {end_time}")
                     for device in data['stats']:
                         #print(f"min time_stamp: {data['stats'][device]['time_stamp'].min()}, max time_stamp: {data['stats'][device]['time_stamp'].max()}")
                         stats_df_window[device] = data['stats'][device][(data['stats'][device]['time_stamp'] >= start_time) & (data['stats'][device]['time_stamp'] < end_time)]
